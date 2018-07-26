45123735aeecb4faa04dba88101668e6a30011d3
Daniel Dragic <danac@guaranteed.site>
2018-07-27 01:50:56 +0200

    request format change

--- Model/Api/DiscountCodeValidation.php
+++ Model/Api/DiscountCodeValidation.php
@@ -1,19 +1,19 @@
 <?php
 /**
-* Bolt magento2 plugin
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Open Software License (OSL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/osl-3.0.php
-*
-* @category   Bolt
-* @package    Bolt_Boltpay
-* @copyright  Copyright (c) 2018 Bolt Financial, Inc (https://www.bolt.com)
-* @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
-*/
+ * Bolt magento2 plugin
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Bolt
+ * @package    Bolt_Boltpay
+ * @copyright  Copyright (c) 2018 Bolt Financial, Inc (https://www.bolt.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
 namespace Bolt\Boltpay\Model\Api;
 
@@ -188,7 +188,7 @@ class DiscountCodeValidation implements DiscountCodeValidationInterface
             $request = json_decode($this->request->getContent());
 
             // get the coupon code
-            $couponCode = trim($request->discount_code);
+            $couponCode = trim($request->cart->discount_code);
 
             // check if empty coupon was sent
             if ($couponCode === '') {
@@ -402,7 +402,7 @@ class DiscountCodeValidation implements DiscountCodeValidationInterface
                 self::ERR_SERVICE,
                 $e->getMessage(),
                 $e->getHttpCode(),
-                $immutableQuote
+                @$immutableQuote
             );
         } catch (\Exception $e) {
             $this->bugsnag->notifyException($e);
@@ -411,7 +411,7 @@ class DiscountCodeValidation implements DiscountCodeValidationInterface
                 self::ERR_SERVICE,
                 $errMsg,
                 422,
-                $immutableQuote
+                @$immutableQuote
             );
         }
     }
