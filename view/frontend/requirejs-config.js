3fb5d6085f1ff0587c64ad361375410077bf9836
Daniel Dragic <danac@guaranteed.site>
2018-08-27 23:14:47 +0200

    Magento v2.0-2.1.x page cache fix include

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -18,10 +18,10 @@
  * Replaces page-cache javascript.
  */
 var config = {
-    // map: {
-    //     '*': {
-    //         pageCache: 'Bolt_Boltpay/js/page-cache',
-    //         'Magento_PageCache/js/page-cache': 'Bolt_Boltpay/js/page-cache'
-    //     }
-    // }
+    map: {
+        '*': {
+            pageCache: 'Bolt_Boltpay/js/page-cache',
+            'Magento_PageCache/js/page-cache': 'Bolt_Boltpay/js/page-cache'
+        }
+    }
 };
